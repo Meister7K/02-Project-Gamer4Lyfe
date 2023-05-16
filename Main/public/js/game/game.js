763896c1b7ae38fefac1e93048c1d484e62d30cd
Masterchef842 <robertwilliamlord55@gmail.com>
2023-05-16 14:10:34 -0500

    small changes to animation system

--- Main/public/js/game/game.js
+++ Main/public/js/game/game.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+
 //console.log(ctx);
-const CANVAS_WIDTH = canvas.width = xxx;
-const CANVAS_HEIGHT = canvas.height = xxx;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 480;
 let gameSpeed = xxx;
 
 const backgroundLayer1 = new Image();
