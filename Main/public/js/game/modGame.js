7bbbc57c0829b13795a420fd7b9d19641de3e177
Masterchef842 <robertwilliamlord55@gmail.com>
2023-05-18 15:58:17 -0500

    added player

--- Main/public/js/game/modGame.js
+++ Main/public/js/game/modGame.js
@@ -1,12 +1,13 @@
-                                                //
-                                                // select and set canvas                 
-                                                //TODO Render array into block objects
-                                                //          -create block class
-                                                //TODO Create player functionality
-                                                //           -create movement/collision/attack/interact/health
-                                                //TODO implement phazer collision detect
-                                                //TODO implement Enemies
-                                                //TODO map change & saving
+//
+// select and set canvas                 
+//? Render array into block objects X
+//          -create block class
+//? Create player functionality
+//           -create movement/collision/attack/interact/health
+//TODO implement block types
+//TODO implement phazer collision detect
+//TODO implement Enemies
+//TODO map change & saving
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");//
 const CANVAS_WIDTH = (canvas.width = 3200);
@@ -21,39 +22,42 @@ const gravity = 1.5;
 //collisions1[x][y] if=1 stop if 2 changelevel 3
 
 class Block {
-    static w = 64;
-    static h = 64;
-    constructor(x, y, num) {
-        this.x = x
-        this.y = y
-        this.num = num
+    
+    constructor(x,y,type,size=64) {
+
+        this.size = size;
+        this.x=x*size;
+        this.y=y*size;
+        this.width=size;
+        this.height=size;
+        this.type = type;
 
     }
+
 }
-const wallBlock = new Block()
 
 class Character {
-    constructor(x, y, w, h, health, sprite) {
+    constructor(x, y, w, h /*,health, sprite*/) {
         this.x = x;
         this.y = y;
         this.velocity = {
             x: 0,
             y: 0,
         };
-        this.sprite = sprite;
         this.width = w;
         this.height = h;
-        this.health = health
+       // this.health = health;
+       //this.sprite = sprite;
     }
     draw() {
         ctx.fillStyle = "red";
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     update() {
         this.draw();
-        this.position.y += this.velocity.y;
-        this.position.x += this.velocity.x;
-        if (this.position.y + this.height + this.velocity.y <= CANVAS_HEIGHT) {
+        this.y += this.velocity.y;
+        this.x += this.velocity.x;
+        if (this.y + this.height + this.velocity.y <= CANVAS_HEIGHT) {
             this.velocity.y += gravity;
         } else {
             this.velocity.y = 0;
@@ -61,19 +65,24 @@ class Character {
     }
 }
 class Player extends Character {
-    super(x, y, w, h, sprite)
+    constructor() {
+        super(x, y, w, h, sprite="")
+    }
 }
 
 class Enemy extends Character {
-    super(x, y, w, h, sprite)
+    constructor() {
+        super(x, y, w, h, sprite)
+    }
+
 }
 
 class Controller {
     constructor(aPlayer) {
         this.player = aPlayer
-        this.right = false,
-        this.left = false,
-        this.interact = false,
+        this.right = false
+        this.left = false
+        this.interact = false
         this.renderController()
     }
     renderController() {
@@ -124,7 +133,6 @@ class Controller {
             case 38 || 87:
                 console.log("up");
                 break;
-
             case 40 || 83:
                 this.interact = false;
                 this.updatePlayer();
@@ -147,18 +155,16 @@ class Controller {
                 console.log("pause");
         }
     }
-    updatePlayer(){
+    updatePlayer() {
         const ps = 10;
-    
-        if(this.right())
-            this.player.velocity.x=ps;
-        if(this.left())
-            this.player.velocity.x=-ps;
-        else{
+
+        if (this.right)
+            this.player.velocity.x = ps;
+        else if (this.left)
+            this.player.velocity.x = -ps;
+        else {
             this.player.velocity.x = 0;
-        }    
-       
-        
+        }
     }
 }
 
@@ -175,43 +181,76 @@ class Level {
         this.X = 0;
         this.Y = 0;
         this.imgObj = this.renderImg();
-        this.colMap = colMap
-
+        this.colMap = colMap;
+        this.blockMap=this.renderBlockMap()
+    }
+    
+    draw() {
+        ctx.drawImage(this.imgObj, this.X, this.Y)
+        for( let i=0;i<this.blockMap.length;i++){
+            
+            ctx.fillStyle = "blue";
+            ctx.fillRect(this.blockMap[i].x, this.blockMap[i].y, this.blockMap[i].width, this.blockMap[i].height);
+        }
+    
+    }
+    renderBlockMap() {
+        let blockArray=[]        
+        this.colMap.forEach((row, i) => {
+            row.forEach((num, j) => {
+                if(num===1 || num === 3 ){
+                    let block=new Block(j,i,'sblock')
+                    blockArray.push(block)
+                }
+                else if( num===2){
+                    let block=new Block(j,i, 'eblock')
+                    blockArray.push(block);
+                }
+                })
+        })
+        return blockArray;
     }
     renderImg() {
         const backgroundImage = new Image()
         backgroundImage.src = this.imgUrl;
-        console.log('bkg' + backgroundImage);
+        // this.blockMap=this.renderBlockMap()
         return backgroundImage;
-
-
     }
-    draw() {
-        ctx.drawImage(this.imgObj, this.X, this.Y)
-        console.log('draw' + this.imgObj);
-    }
-
 }
 
 
 
-function loadLevel(map/*,player,*/) {
+function loadLevel (map) {
     map.imgObj.onload = () => {
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         map.draw()
-    }
-    // add background/map img on load then draw
+        console.log('map' + map)
 
-}
+    
+    }
+}  
+    // add background/map img on load then draw
+    
 
 
 
 
 //Active Code Below
+
 const level1 = new Level("../images/DungeonRoom2.png", collisions01)
+const character1 = new Character(700,1000,16,32);
+const controller= new Controller(character1)
+console.log('char' + character1);
+
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    loadLevel(level1)
+    level1.draw()
+    character1.update()
+    //loadLevel(level1,character1)
+    requestAnimationFrame(animate);
 }
+loadLevel(level1)//if statement needed for switch ing levels on reaching animate 
 animate()
 
 
